803c347ae82a6e3ca74d3f08731b820ce23c37d0
Edoardo Del Sarto <e99delsarto@gmail.com>
2026-05-12 17:54:47 +0200

    Esempio @Autowired

--- 17_Martedi_12_05/demo/src/main/java/com/example/demo/DemoApplication.java
+++ 17_Martedi_12_05/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 
 import com.example.demo.Run.Location;
 import com.example.demo.Run.RunRecord;
@@ -16,17 +18,25 @@ import com.example.demo.Services.OrderService;
 @SpringBootApplication
 public class DemoApplication {
 
+	@Autowired
+	private OrderService orderService;
+
+	// @Autowired
+	// public void setOrderService(OrderService orderService){
+	// 	this.orderService = orderService;
+	// }
+
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
 
-		System.out.println("=== BEAN REGISTRATI ===");
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		for(String name : beanNames){
-			if(!name.contains("org.springframework"))
-			{
-				System.out.println(" - " + name);
-			}
-		}
+		// System.out.println("=== BEAN REGISTRATI ===");
+		// String[] beanNames = ctx.getBeanDefinitionNames();
+		// for(String name : beanNames){
+		// 	if(!name.contains("org.springframework"))
+		// 	{
+		// 		System.out.println(" - " + name);
+		// 	}
+		// }
 
 		// WelcomeMessage msg = new WelcomeMessage();
 		// System.out.println(msg.getMessage());
@@ -34,19 +44,19 @@ public class DemoApplication {
 
 	}
 
-	// @Bean
-	// CommandLineRunner runner(OrderService orderService){
-	// 	return args -> {
-	// 		// RunRecord run = new RunRecord(124, 
-	// 		// 						"Corsa pomeridiana", 
-	// 		// 						LocalDateTime.now(), 
-	// 		// 						LocalDateTime.now().plus(1, ChronoUnit.HOURS),
-	// 		// 						5,
-	// 		// 						Location.INDOOR);
-	// 		// System.out.println("Run " + run);
-	// 		orderService.processOrder("ORD-001");
-	// 		orderService.processOrder("ORD-002");
-	// 	};
-	// }
+	@Bean
+	CommandLineRunner runner(OrderService orderService){
+		return args -> {
+			// RunRecord run = new RunRecord(124, 
+			// 						"Corsa pomeridiana", 
+			// 						LocalDateTime.now(), 
+			// 						LocalDateTime.now().plus(1, ChronoUnit.HOURS),
+			// 						5,
+			// 						Location.INDOOR);
+			// System.out.println("Run " + run);
+			orderService.processOrder("ORD-001");
+			orderService.processOrder("ORD-002");
+		};
+	}
 
 }
--- 17_Martedi_12_05/demo/src/main/java/com/example/demo/Services/OrderService.java
+++ 17_Martedi_12_05/demo/src/main/java/com/example/demo/Services/OrderService.java
@@ -1,15 +1,24 @@
 package com.example.demo.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
     
-    private final NotificationService notificationService;
+    // private final NotificationService notificationService;
 
-    public OrderService (NotificationService notificationService){
-        this.notificationService = notificationService;
-    }
+    // public OrderService (NotificationService notificationService){
+    //     this.notificationService = notificationService;
+    // }
+
+    @Autowired
+    private NotificationService notificationService;
+    
+    // @Autowired
+    // public void setNotificationService(NotificationService notificationService){
+    //     this.notificationService = notificationService;
+    // }
 
     public void processOrder(String OrderId){
         System.out.println("Elaborazione ordine: " + OrderId);
